8b0739e12740854994e05cb5b6925420da1f8bcc
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2024-09-28 21:31:27 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+pkg install python -y
+pkg install python3 -y
+pkg install python git -y
+pkg install termux-api
+pip install re
+pip install colorama
+
+clear
+echo -e "\n\n\033[0;33mPlease Install Termux API manually From Google using F-driod\033[0m"
+echo -e "\n\n\033[0;32mRun This Command :- python smsbomber.py\033[0m"
